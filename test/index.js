c987af081a8efaf3915571e6722697d884d82d3a
bouzuya <bouzuya@gmail.com>
2014-01-04 14:55:45 +0900

    fix test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
-var expect = require('expect.js');
+var expect = require('chai').expect;
 var add = require('../');
 
 describe('1 + 2', function() {
   it('=3', function(done) {
-    expect(add(1, 2)).to.be(3);
+    expect(add(1, 2)).to.equal(3);
     done();
   });
 });
